12b1ed505045493731b11f8043d8800e4075261e
Vishnu Methre <vmethre@manh.com>
2023-02-15 23:40:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let val = document.getElementById('vishnu');
+console.log(val.innerText);
+
+let val2 = document.getElementById('rd');
+val2.innerHTML=`<p>my name is manhattan and .........</p>`
+console.log(val2.innerHTML);
